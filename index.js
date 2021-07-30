4f609817f34f6ae2c61daef43164e61d7c67c3d9
GoldenPavilion <rcorystine@gmail.com>
2021-07-30 12:34:33 -0400

    created submit data function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email){
+    const formData = {
+        name: name,
+        email: email
+    };
+    
+const configObj = {
+    method = "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+    
+return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object);
+    });
+}
